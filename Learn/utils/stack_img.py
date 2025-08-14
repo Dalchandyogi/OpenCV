0a0633ce4922d4d10a0e7014495c5e2dca79fd62
Dal Chand Yogi <dalchandyogi08@gmail.com>
2025-08-14 15:39:43 +0530

    add some utils function.

---
+++ Learn/utils/stack_img.py
@@ -0,0 +1,34 @@
+import  cv2
+import numpy as np
+
+def stack_image(scale, images):
+    rows = len(images)
+    cols = len(images[0])
+    rows_available = isinstance(images[0], list)
+    width = images[0][0].shape[1]
+    height = images[0][0].shape[0]
+    if rows_available:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if images[x][y].shape[:2] == images[0][0].shape[:2]:
+                    images[x][y] = cv2.resize(images[x][y], (0, 0), None, scale, scale)
+                else:
+                    images[x][y] = cv2.resize(images[x][y], (images[0][0].shape[1], images[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(images[x][y].shape) == 2: images[x][y] = cv2.cvtColor(images[x][y], cv2.COLOR_GRAY2BGR)
+        image_blank = np.zeros((height, width, 3), np.uint8)
+        hor = [image_blank] * rows
+        hor_con = [image_blank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(images[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if images[x].shape[:2] == images[0].shape[:2]:
+                images[x] = cv2.resize(images[x], (0, 0), None, scale, scale)
+            else:
+                images[x] = cv2.resize(images[x], (images[0].shape[1], images[0].shape[0]), None, scale, scale)
+            if len(images[x].shape) == 2: images[x] = cv2.cvtColor(images[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(images)
+        ver = hor
+    return ver
